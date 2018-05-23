6d18e7d4207948f8636a38b56e58e7fee2f9be31
Raquel <raquel.ruiz.orozco@alumnos.upm.es>
2018-05-23 12:44:24 +0200

    p6.1

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -158,52 +158,48 @@ exports.randomplay = (req, res, next) => {
     if(req.session.resolved === undefined){
         req.session.resolved = [];
     };
-    const Op= Sequelize.Op;
-
-
-    const condicion = {"id":{[Op.notIn]:req.session.resolved}};
-
-    return models.quiz.count({where: condicion}).then(count => {
-
-        return models.quiz.findAll({
-        offset:Math.floor(Math.random()*count),
-        limit:1,
-        where: condicion
-
-    }).then(quizzes =>{
-        return quizzes[0];
-});
-
-})
-
-.catch(error => next(error));
-
-
-
-.then(quiz =>{
-        let score = req.session.resolved.length;
-    if(quiz ===undefined){
-        delete req.session.resolved;
-        res.render('quizzes/random_nomore', {score});
-
-    }else{
-        res.render('quizzes/random_play', {
-            quiz, score
+    Sequelize.Promise.resolve().then(() =>{
+        const whereOpt = {"id":{[Sequelize.Op.notIn]:req.session.resolved}};
+        return models.quiz.count({where: whereOpt}).then(count => {
+            let ran = Math.floor(Math.random()*count);
+            return models.quiz.findAll({
+                offset:ran,
+                limit:1,
+                where: whereOpt
+            }).then(quizzes =>{
+                return quizzes[0];
+            });
+        }).catch(error => {
+            req.flash('error', 'Error deleting the Quiz: ' + error.message);
+            next(error);
         });
-    }
+    }).then(quiz =>{
+        let score = req.session.resolved.length;
+        if(quiz ===undefined){
+            delete req.session.resolved;
+            res.render('quizzes/random_nomore', {score});
 
-});
+        }else{
+            res.render('quizzes/random_play', {
+                quiz, score
+            });
+        }
+
+    });
 };
 
 
 
-
 exports.randomcheck = (req, res, next) =>{
+    if(req.session.resolved === undefined){
+        req.session.resolved = [];
+    }
 
-    var score = req.session.resolved.length;
+    let score = req.session.resolved.length;
     const answer = req.query.answer;
 
     const resultado = answer.toLowerCase().trim() === req.quiz.answer.toLowerCase().trim();
+
     if(resultado){
         if(req.session.resolved.indexOf(req.quiz.id)=== -1){
             req.session.resolved.push(req.quiz.id);
